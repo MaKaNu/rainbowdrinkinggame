9fd0f5d9c2296d15a2a8726d63891473017ed982
Matti <matti.kaupenjohann@fh-dortmund.de>
2022-04-16 18:34:44 +0200

    Add docstrings:
    Modules:
    - utils
    Functions:
    - utils.create_markdown
    - utils.export_markdown

--- rainbowdrinkinggame/utils.py
+++ rainbowdrinkinggame/utils.py
@@ -1,11 +1,35 @@
-"""Util methods"""
+# -*- coding: utf-8 -*-
+"""Rainbow Drinking Game Util Functions
+
+This Module includes function which will be utilized by the Bridge Slot Methods.
+The functions are mainly used inside slots methods, but can also be used outside
+the methods.
+
+Todo:
+    - All Fine for the moment.
+"""
 from pathlib import Path
 from typing import Dict, Union, List
 
 from markdown import markdown
 
 
-def create_markdown(player_values:Dict[str, Union[List[str], List[int]]]):
+def create_markdown(player_values:Dict[str, Union[List[str], List[int]]]) -> str:
+    """This Function creates a multiline string based on a dictinary
+    
+    The dictionary has to follow some strict rules,
+    since the game "rainbowdrinkinggame allows only 5 Players. 
+
+    Args:
+        player_values (Dict[str, Union[List[str], List[int]]]): The dictinary which includes
+            the player values:
+                - PlayerNames
+                - PlayerSips
+                - PlayerShots
+            
+    Returns:
+        str: a multiline markdownstyle str
+    """
     assert len(player_values['players']) == 5, "Player Count needs to be 5"
     assert len(player_values['players']) == len(player_values['sips']), f"Sips count needs to be equal player count"
     assert len(player_values['players']) == len(player_values['shots']), f"Shots count needs to be equal player count"
@@ -41,7 +65,13 @@ def create_markdown(player_values:Dict[str, Union[List[str], List[int]]]):
 * {player_values['players'][shot_sorted_idx[4]]}: {shots_sorted[4]}
     '''
     
-def export_markdown(md_str:str, path:Path):
+def export_markdown(md_str:str, path:Path)->None:
+    """Function to export a given markdown string to given path.
+
+    Args:
+        md_str (str): A String in Markdown Format
+        path (Path): Path variable for the html output
+    """
     html = markdown(md_str)
     with open(path, 'w') as f:
         f.write(html)
