49ac0a7731e384fb0fcdee81d04691714c54c683
Matti <matti.kaupenjohann@fh-dortmund.de>
2022-04-24 18:46:22 +0200

    black formatted tests

--- tests/test_rainbowdrinkinggame.py
+++ tests/test_rainbowdrinkinggame.py
@@ -1,14 +1,10 @@
 """Test module for rainbowdrinkinggame."""
 
-from rainbowdrinkinggame import (
-    __author__,
-    __email__,
-    __version__
-)
+from rainbowdrinkinggame import __author__, __email__, __version__
 from rainbowdrinkinggame.rainbowdrinkinggame import (
     QML_IMPORT_NAME,
     QML_IMPORT_MAJOR_VERSION,
-    Bridge
+    Bridge,
 )
 
 
@@ -18,17 +14,19 @@ def test_project_info():
     assert __email__ == "matti.kaupenjohann@fh-dortmund.de"
     assert __version__ == "0.0.0"
 
+
 def test_qml_globals():
-    """Test QML Specific Global Variables
-    """
+    """Test QML Specific Global Variables"""
     assert QML_IMPORT_MAJOR_VERSION == 1
     assert QML_IMPORT_NAME == "io.qt.textproperties"
+
 
 def test_bridge_plus():
     bridge = Bridge()
     output = bridge.plus(1)
     assert output == "2"
     assert isinstance(output, str)
+
 
 def test_bridge_minus():
     bridge = Bridge()
@@ -38,18 +36,10 @@ def test_bridge_minus():
     assert output_zero == "0"
     assert isinstance(output, str)
 
+
 def test_bridge_create_result(mocker):
-    mocker.patch(
-        'rainbowdrinkinggame.utils.create_markdown',
-        """# HEADER""")
-    mocker.patch(
-        'rainbowdrinkinggame.utils.export_markdown',
-        None
-    )
+    mocker.patch("rainbowdrinkinggame.utils.create_markdown", """# HEADER""")
+    mocker.patch("rainbowdrinkinggame.utils.export_markdown", None)
     bridge = Bridge()
-    output = bridge.create_result(
-        ('P1', 'P2', 'P3', 'P4', 'P5'),
-        (1,1,1,1,1),
-        (1,1,1,1,1)
-    )
+    output = bridge.create_result(("P1", "P2", "P3", "P4", "P5"), (1, 1, 1, 1, 1), (1, 1, 1, 1, 1))
     assert output is None
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,42 +8,46 @@ from rainbowdrinkinggame.utils import create_markdown, export_markdown
 
 def test_create_markdown_generel():
     input_dict = {
-        'players': ['P1', 'P2', 'P3', 'P4', 'P5'],
-        'sips': [1,2,3,4,5],
-        'shots': [5,4,3,2,1]
-        }
+        "players": ["P1", "P2", "P3", "P4", "P5"],
+        "sips": [1, 2, 3, 4, 5],
+        "shots": [5, 4, 3, 2, 1],
+    }
     output = create_markdown(input_dict)
     assert isinstance(output, str)
 
+
 def test_create_markdown_less_players():
     input_dict = {
-        'players': ['P1', 'P2', 'P3', 'P4'],
-        'sips': [1,2,3,4,5],
-        'shots': [5,4,3,2,1]
-        }
+        "players": ["P1", "P2", "P3", "P4"],
+        "sips": [1, 2, 3, 4, 5],
+        "shots": [5, 4, 3, 2, 1],
+    }
     with pytest.raises(AssertionError) as excinfo:
         create_markdown(input_dict)
     assert "Player Count needs to be 5" in str(excinfo.value)
 
+
 def test_create_markdown_less_sips():
     input_dict = {
-        'players': ['P1', 'P2', 'P3', 'P4', 'P5'],
-        'sips': [1,2,3,4],
-        'shots': [5,4,3,2,1]
-        }
+        "players": ["P1", "P2", "P3", "P4", "P5"],
+        "sips": [1, 2, 3, 4],
+        "shots": [5, 4, 3, 2, 1],
+    }
     with pytest.raises(AssertionError) as excinfo:
         create_markdown(input_dict)
     assert "Sips count needs to be equal player count" in str(excinfo.value)
 
+
 def test_create_markdown_less_shots():
     input_dict = {
-        'players': ['P1', 'P2', 'P3', 'P4', 'P5'],
-        'sips': [1,2,3,4,5],
-        'shots': [5,4,3,2]
-        }
+        "players": ["P1", "P2", "P3", "P4", "P5"],
+        "sips": [1, 2, 3, 4, 5],
+        "shots": [5, 4, 3, 2],
+    }
     with pytest.raises(AssertionError) as excinfo:
         create_markdown(input_dict)
     assert "Shots count needs to be equal player count" in str(excinfo.value)
+
 
 def test_export_markdown_generel(fs):
     input_str = """
@@ -54,12 +58,11 @@ def test_export_markdown_generel(fs):
 
 test text
     """
-    directory = Path('/test/test.html')
+    directory = Path("/test/test.html")
     fs.create_file(directory)
     assert directory.exists()
-    with open(directory, 'r', encoding="utf8") as file:
+    with open(directory, "r", encoding="utf8") as file:
         assert len(file.readlines()) == 0
     export_markdown(input_str, directory)
-    with open(directory, 'r', encoding="utf8") as file:
+    with open(directory, "r", encoding="utf8") as file:
         assert len(file.readlines()) > 0
-        
